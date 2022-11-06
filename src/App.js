77380476c57894c5e034c47ebdda3645a978c120
aimtraders <amrendras081@gmail.com>
2022-11-06 22:56:30 +0530

    6nov

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactDOM } from 'react-dom';
+import { BrowserRouter as Router,Routes, Route, Link, Switch } from 'react-router-dom';
 
+import signin from './Component/signin.component';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+      <Route exact path="/" component={signin} />
+    </Switch>
+    </Router>
   );
 }
 
---
+++ src/Component/login.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import BGimg from '../images/Rectangle 1 (2).jpeg'
+function register() {
+  return (
+    <div className='.RegisterPg'>
+
+      <div className='GSLeft'>
+
+        <div className='PgNo'>01</div>
+        <div className='PgNo'>Create Music</div>
+        <span>Lorem ipsum dolor sit amet,  Est accusantium saepe cum aliquid aspernatur ex! Officiis cumque iusto porro sed distinctio.</span>
+        <div style={{ display: "flex" }}>
+          <div className='dotWhite'></div>
+          <div className='dotGray'></div>
+          <div className='dotGray'></div>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Register</h5>
+          <div className="GSintro">leLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consectetur euismod.</div>
+          <input type="text" />
+          <input type="Email" />
+          <input type="Password" />
+          <input type="Password" />
+          <p>I Agree to Terms&Conditions And Privacy Policy</p>
+          <button className='continueBtn'>CONTINUE</button>
+          <p>Already Have an account? <a href="">Sign in</a></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default register;
